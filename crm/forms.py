9fe3b6c00726a483bbbb05e18ff94e0da806bcad
babonaresh <nbashaboena@gmail.com>
2018-09-18 17:53:50 -0500

    updated

--- crm/forms.py
+++ crm/forms.py
@@ -17,25 +17,3 @@ class ProductForm(forms.ModelForm):
        model = Product
        fields = ('cust_name', 'product', 'p_description', 'quantity', 'pickup_time', 'charge' )
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password',
-                                widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'email')
-
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.')
-        return cd['password2']
-
-class EmailPostForm(forms.Form):
-        name = forms.CharField(max_length=25)
-        email = forms.EmailField()
-        to = forms.EmailField()
-        comments = forms.CharField(required=False,
-                                   widget=forms.Textarea)
